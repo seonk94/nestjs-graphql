fec7181345ae6285f7c74d8b1a093f2c6e868906
sk <seonk94@naver.com>
2021-06-02 23:40:20 +0900

    feat: 물결 animation 생성

---
+++ src/assets/style/animation/index.ts
@@ -0,0 +1,37 @@
+import { keyframes } from 'styled-components';
+
+export const wave = keyframes`
+  0% {
+    background-position: left 0px top 0px;
+  }
+  50% { 
+    background-position: left 100% top 100%;
+  }
+  100% {
+    background-position: left 0px top 0px;
+  } 
+`;
+
+export const infiniteWave = keyframes`
+  0% {
+      background-position: left 0px bottom -80px;
+  }
+  100% { 
+      background-position: left 1500px bottom 20px;
+  }
+`;
+
+export const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const drift = keyframes`
+  from { transform: rotate(0deg); }
+  from { transform: rotate(360deg); }
+`;
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import { drift } from 'src/assets/style/animation';
+import styled from 'styled-components';
+
+interface WavePropType {
+  time: string;
+  color: string;
+  opacity: string;
+}
+
+const Container = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+const WaveContainer = styled.div`
+  flex-grow: 1;
+  width: 100vw;
+  height: 100vh;
+  box-shadow: 0 2px 30px rgba(black, .2);
+  background: lighten(#f0f4c3, 10%);
+  position: fixed;
+  overflow: hidden;
+  transform: translate3d(0, 0, 0);
+`;
+
+const Wave = styled.div<WavePropType>`
+  position: absolute;
+  width: 300vh;
+  left: 50%;
+  bottom: 50%;
+  height: 300vh;
+  margin-left: -150vh;
+  margin-right: -150vh;
+  transform-origin: 50% 48%;
+  border-radius: 43%;
+  opacity: ${(props) => props.opacity};
+  background: ${(props) => props.color};
+  animation: ${drift} ${(props) => props.time} infinite linear;
+`;
+
+const Title = styled.p`
+  text-align: center;
+  color: cyan;
+  font-size: 5em;
+  text-transform: uppercase;
+  letter-spacing: .3em;
+`;
 
 const Home = () => (
-  <div>
-    home  
-  </div>
-    
+  <Container>
+    <Title>See Mind</Title>
+    <WaveContainer>
+      <Wave time="3000ms" opacity=".4" color="pink" />
+      <Wave time="5000ms" opacity=".1" color="coral" />
+      <Wave time="7000ms" opacity=".4" color="aqua" />
+    </WaveContainer>
+  </Container>
 );
 export default Home;
